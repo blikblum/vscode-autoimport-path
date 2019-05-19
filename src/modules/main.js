d6d43fc7a145a63582e076f3e78bd02b19be3b22
Luiz Américo <luizamericop@gmail.com>
2019-05-19 13:27:39 -0300

    Remove import statements

--- src/modules/main.js
+++ src/modules/main.js
@@ -1,8 +1,4 @@
-import { otherService } from "other/otherservice";
-import { myService } from "myservice";
-import { Home } from "src/home/home";
-
 // try to auto import below identifiers (Ctrl + Space)
-myService
-otherService
+myS
+otherS
 Ho
